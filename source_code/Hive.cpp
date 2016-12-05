96b9a68568f032a83da577d53802c2fde2bb1d29
Tim Z <zhenghanecho@gmail.com>
2016-12-05 09:10:54 +0800

    Revert commit

--- source_code/Hive.cpp
+++ source_code/Hive.cpp
@@ -5,45 +5,43 @@ Hive::Hive(QObject *parent) : QObject(parent)
 
 #ifdef Q_OS_OSX
 
-  QApplication::setQuitOnLastWindowClosed(false);
-  QtMac::setBadgeLabelText("Hi");
+	QApplication::setQuitOnLastWindowClosed(false);
+	QtMac::setBadgeLabelText("Hi");
 #endif
 
 	qDebug()<<this->thread()->currentThreadId();
 
 	thread_data = new ThreadData();
-	QThread *data_thread = new QThread(this);
-	thread_data->moveToThread(data_thread);
+
 
 	thread_net = new ThreadNet();
-	QThread *net_thread = new QThread(this);
-	thread_net->moveToThread(net_thread);
 
-  gui_central_widget = new GuiCentralWidget();
-  //QObject not compatible to QWidget para, delete obj manually
 
-  ////connect
+	gui_central_widget = new GuiCentralWidget();
+	//QObject not compatible to QWidget para, delete obj manually
+
+	////connect
 	connect(thread_data, &ThreadData::updatesAvailable,
 					gui_central_widget, &GuiCentralWidget::onUpdateAvailable,
 					Qt::QueuedConnection);
-
-  connect(thread_net, &ThreadNet::usrEnter,
-					thread_data, &ThreadData::onUsrEntered,
-					Qt::QueuedConnection);
-  connect(thread_net, &ThreadNet::updateAvailable,
+	connect(thread_net, &ThreadNet::updateAvailable,
 					thread_data, &ThreadData::onUpdatesAvailable,
 					Qt::QueuedConnection);
-  connect(thread_data, &ThreadData::usrProfileLoaded,
+
+	connect(thread_net, &ThreadNet::usrEnter,
+					thread_data, &ThreadData::onUsrEntered,
+					Qt::QueuedConnection);
+	connect(thread_data, &ThreadData::usrProfileLoaded,
 					gui_central_widget, &GuiCentralWidget::addUsr,
 					Qt::QueuedConnection);
-  connect(thread_data, &ThreadData::usrProfileChanged,
+	connect(thread_data, &ThreadData::usrProfileChanged,
 					gui_central_widget, &GuiCentralWidget::changeUsr,
 					Qt::QueuedConnection);
 
-  connect(gui_central_widget->gui_main_block->gui_chat_stack, &GuiChatStack::sendMessage,
+	connect(gui_central_widget->gui_main_block->gui_chat_stack, &GuiChatStack::sendMessage,
 					this, &Hive::onTextMessageToSend,
 					Qt::QueuedConnection);
-  connect(thread_net, &ThreadNet::messageRecieved,
+	connect(thread_net, &ThreadNet::messageRecieved,
 					thread_data, &ThreadData::onMessageCome,
 					Qt::QueuedConnection);
 	connect(thread_data, &ThreadData::messageLoaded,
@@ -51,11 +49,12 @@ Hive::Hive(QObject *parent) : QObject(parent)
 					Qt::QueuedConnection);
 
 
+
 	thread_data->start(QThread::NormalPriority);
 	thread_net->start(QThread::NormalPriority);
 
 #ifdef Q_OS_OSX
-  QtMac::setBadgeLabelText("");
+	QtMac::setBadgeLabelText("");
 #endif
 }
 
@@ -63,57 +62,57 @@ Hive::~Hive()
 {
 #ifdef Q_OS_OSX
 
-  QtMac::setBadgeLabelText("Bye");
+	QtMac::setBadgeLabelText("Bye");
 #endif
 
-  gui_central_widget->close();
-  gui_central_widget->deleteLater();
+	gui_central_widget->close();
+	gui_central_widget->deleteLater();
 
-  thread_net->quit();
-  thread_data->quit();
-  if(!thread_net->wait(500))
-    {
-      thread_net->terminate();
-      thread_net->wait();
-    }
-  if(!thread_data->wait(500))
-    {
-      thread_data->terminate();
-      thread_data->wait();
-    }
+	thread_net->quit();
+	thread_data->quit();
+	if(!thread_net->wait(500))
+		{
+			thread_net->terminate();
+			thread_net->wait();
+		}
+	if(!thread_data->wait(500))
+		{
+			thread_data->terminate();
+			thread_data->wait();
+		}
 }
 
 
 void Hive::onTextMessageToSend(const QString &receiver, const QString &message)
 {
-  QJsonObject json_object;
-  json_object.insert("sender", GlobalData::settings_struct.profile_key_str);
-  json_object.insert("receiver", receiver);
-  json_object.insert("time", GlobalData::getCurrentTime());
-  json_object.insert("message", message);
+	QJsonObject json_object;
+	json_object.insert("sender", GlobalData::settings_struct.profile_key_str);
+	json_object.insert("receiver", receiver);
+	json_object.insert("time", GlobalData::getCurrentTime());
+	json_object.insert("message", message);
 
-  thread_net->udpSendMessage(json_object);
+	thread_net->udpSendMessage(json_object);
 }
 
 QJsonObject Hive::wrapTextMessage(const Message::TextMessageStruct &messageStruct)
 {
-  QJsonObject json_object;
-  json_object.insert("sender", messageStruct.sender);
-  json_object.insert("receiver", messageStruct.reciever);
-  json_object.insert("time", messageStruct.time);
-  json_object.insert("message", messageStruct.message);
+	QJsonObject json_object;
+	json_object.insert("sender", messageStruct.sender);
+	json_object.insert("receiver", messageStruct.reciever);
+	json_object.insert("time", messageStruct.time);
+	json_object.insert("message", messageStruct.message);
 
-  return json_object;
+	return json_object;
 }
 
 QJsonObject Hive::wrapFileMessage(const Message::FileInfoStruct &fileInfoStruct)
 {
-  QJsonObject json_object;
-  json_object.insert("name", fileInfoStruct.name);
-  json_object.insert("size", fileInfoStruct.size);
-  json_object.insert("type", fileInfoStruct.type);
+	QJsonObject json_object;
+	json_object.insert("name", fileInfoStruct.name);
+	json_object.insert("size", fileInfoStruct.size);
+	json_object.insert("type", fileInfoStruct.type);
 
-  return json_object;
+	return json_object;
 }
 
 
