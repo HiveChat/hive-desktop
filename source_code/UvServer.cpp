8587b9907efb0b5c6b1cb93efb7395762c56bad8
Tim Z <zhenghanecho@gmail.com>
2017-12-21 17:39:33 -0500

    Prepare for backup branch

--- source_code/UvServer.cpp
+++ source_code/UvServer.cpp
@@ -12,8 +12,8 @@ QHash<QString, UvServer::SocketDescriptor> UvServer::key_sd_hash;
 /// BEGIN: 9 Dec. 2017 Test
 ///  - GitHub Source: ultrasilicon/uv_tcp_echo.c
 ///  - URL: https://gist.github.com/ultrasilicon/f070f7acc4fe0b036d37f9383a484f5c
-bool UvServer::stop = false;
-struct sockaddr_in UvServer::addr;
+//bool UvServer::stop = false;
+//struct sockaddr_in UvServer::addr;
 /// END: 9 Dec. 2017 Test
 
 
@@ -178,119 +178,6 @@ UvServer::run()
 }
 
 void
-UvServer::tcpNewConnectionCb(uv_stream_t *handle, int status)
-{
-  if(status < 0)
-    {
-      fprintf(stderr, "New connection error %s\n", uv_strerror(status));
-      return;
-    }
-
-  uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
-  uv_tcp_init(loop, client);
-
-
-  if(uv_accept(handle, (uv_stream_t*)client) == 0)
-    {
-      uv_read_start((uv_stream_t*)client, allocBuffer, tcpReadCb);
-    }
-  else
-    {
-      uv_close((uv_handle_t*) client, NULL);
-    }
-}
-
-void
-UvServer::tcpReadCb(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf)
-{
-  SocketDescriptor socketDiscriptor = getSocketDescriptor((uv_handle_t *)handle);
-
-  if(nread > 0)
-    {
-      HiveClientBuffer *hiveClient;
-      if(buffer_hash.contains(socketDiscriptor))
-        {
-          Log::net(Log::Normal, "UvServer::tcpRead()", "Reading message form old user: " + QString::number(socketDiscriptor));
-          hiveClient = buffer_hash.value(socketDiscriptor);
-        }
-      else
-        {
-          Log::net(Log::Normal, "UvServer::tcpRead()", "Reading message form new user: " + QString::number(socketDiscriptor));
-          hiveClient = new HiveClientBuffer();
-          buffer_hash.insert(socketDiscriptor, hiveClient);
-        }
-
-      uv_buf_t buffer = uv_buf_init(buf->base, nread);
-      decodeTcp(QString::fromUtf8(buffer.base, buffer.len), hiveClient);
-
-      // << echo server
-      write_req_t *req = (write_req_t*)malloc(sizeof(write_req_t));
-      req->buf = uv_buf_init(buf->base, nread);
-      uv_write((uv_write_t*)req, handle, &req->buf, 1, tcpWriteCb);
-
-      return;
-    }
-  if(nread < 0)
-    {
-      if (nread != UV_EOF)
-        {
-          fprintf(stderr, "TCP Read error %s\n", uv_err_name(nread));
-        }
-      Log::net(Log::Normal, "UvServer::tcpRead()", "Disconnected from discriptor: " + QString::number(socketDiscriptor));
-
-      uv_close((uv_handle_t*)handle, NULL); // NULL is a close callback
-    }
-
-  free(buf->base);
-}
-
-void
-UvServer::tcpWriteCb(uv_write_t *handle, int status)
-{
-  if (status)
-    {
-      fprintf(stderr, "Write error %s\n", uv_strerror(status));
-    }
-
-  freeWriteReq(handle);
-}
-
-void
-UvServer::udpReadCb(uv_udp_t *handle, ssize_t nread, const uv_buf_t *buf, const struct sockaddr *addr, unsigned flags)
-{
-  if(nread >= 0 && addr) //addr sometimes is a nullptr
-    {
-      char senderAddr[17] = { 0 };
-      uv_ip4_name((const struct sockaddr_in*)addr, senderAddr, 16);
-      fprintf(stderr, "Recv from %s\n", senderAddr);
-
-      uv_buf_t buffer = uv_buf_init(buf->base, nread);
-      qDebug()<<"base size: "<< nread<<"base content: "<<QString::fromLatin1(buffer.base);
-      qDebug()<<decodeHivePacket(buffer.base, &senderAddr[0]);
-    }
-  else
-    {
-      fprintf(stderr, "UDP Read error %s\n", uv_err_name(nread));
-      //      uv_close((uv_handle_t*) handle, NULL);
-    }
-
-  free(buf->base);
-  return;
-}
-
-void
-UvServer::udpWriteCb(uv_udp_send_t *handle, int status)
-{
-  if(status)
-    {
-      fprintf(stderr, "Send error %s\n", uv_strerror(status));
-      return;
-    }
-  free(handle); //<<not sure
-  Log::net(Log::Normal, "UvServer::udpWriten()", "UDP message successfully sent!");
-}
-
-void
 UvServer::udpHeartBeatCb(uv_timer_t *handle)
 {
   /// successful test code for timer not crashing
@@ -317,32 +204,145 @@ UvServer::udpHeartBeatCb(uv_timer_t *handle)
 //  Log::net(Log::Normal, "UvServer::udpHeartBeatCb()", "heart beat sent");
 }
 
-void
-UvServer::allocBuffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
-{
-  buf->base = (char*) malloc(suggested_size);
-  buf->len = suggested_size;
-}
+//void
+//UvServer::tcpNewConnectionCb(uv_stream_t *handle, int status)
+//{
+//  if(status < 0)
+//    {
+//      fprintf(stderr, "New connection error %s\n", uv_strerror(status));
+//      return;
+//    }
 
-void
-UvServer::freeWriteReq(uv_write_t *handle)
-{
-  write_req_t *wr = (write_req_t*) handle;
-  free(wr->buf.base);
-  free(wr);
-}
+//  uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+//  uv_tcp_init(loop, client);
 
-int
-UvServer::getSocketDescriptor(uv_handle_t *handle)
-{
-  int fd;
-#ifdef Q_OS_WIN
-  uv_fileno(handle, (uv_os_fd_t*)(&fd));
-#else
-  uv_fileno(handle, &fd);
-#endif
-  return fd;
-}
+
+//  if(uv_accept(handle, (uv_stream_t*)client) == 0)
+//    {
+//      uv_read_start((uv_stream_t*)client, allocBuffer, tcpReadCb);
+//    }
+//  else
+//    {
+//      uv_close((uv_handle_t*) client, NULL);
+//    }
+//}
+
+//void
+//UvServer::tcpReadCb(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf)
+//{
+//  SocketDescriptor socketDiscriptor = getSocketDescriptor((uv_handle_t *)handle);
+
+//  if(nread > 0)
+//    {
+//      HiveClientBuffer *hiveClient;
+//      if(buffer_hash.contains(socketDiscriptor))
+//        {
+//          Log::net(Log::Normal, "UvServer::tcpRead()", "Reading message form old user: " + QString::number(socketDiscriptor));
+//          hiveClient = buffer_hash.value(socketDiscriptor);
+//        }
+//      else
+//        {
+//          Log::net(Log::Normal, "UvServer::tcpRead()", "Reading message form new user: " + QString::number(socketDiscriptor));
+//          hiveClient = new HiveClientBuffer();
+//          buffer_hash.insert(socketDiscriptor, hiveClient);
+//        }
+
+//      uv_buf_t buffer = uv_buf_init(buf->base, nread);
+//      decodeTcp(QString::fromUtf8(buffer.base, buffer.len), hiveClient);
+
+//      // << echo server
+//      write_req_t *req = (write_req_t*)malloc(sizeof(write_req_t));
+//      req->buf = uv_buf_init(buf->base, nread);
+//      uv_write((uv_write_t*)req, handle, &req->buf, 1, tcpWriteCb);
+
+//      return;
+//    }
+//  if(nread < 0)
+//    {
+//      if (nread != UV_EOF)
+//        {
+//          fprintf(stderr, "TCP Read error %s\n", uv_err_name(nread));
+//        }
+//      Log::net(Log::Normal, "UvServer::tcpRead()", "Disconnected from discriptor: " + QString::number(socketDiscriptor));
+
+//      uv_close((uv_handle_t*)handle, NULL); // NULL is a close callback
+//    }
+
+//  free(buf->base);
+//}
+
+//void
+//UvServer::tcpWriteCb(uv_write_t *handle, int status)
+//{
+//  if (status)
+//    {
+//      fprintf(stderr, "Write error %s\n", uv_strerror(status));
+//    }
+
+//  freeWriteReq(handle);
+//}
+
+//void
+//UvServer::udpReadCb(uv_udp_t *handle, ssize_t nread, const uv_buf_t *buf, const struct sockaddr *addr, unsigned flags)
+//{
+//  if(nread >= 0 && addr) //addr sometimes is a nullptr
+//    {
+//      char senderAddr[17] = { 0 };
+//      uv_ip4_name((const struct sockaddr_in*)addr, senderAddr, 16);
+//      fprintf(stderr, "Recv from %s\n", senderAddr);
+
+//      uv_buf_t buffer = uv_buf_init(buf->base, nread);
+//      qDebug()<<"base size: "<< nread<<"base content: "<<QString::fromLatin1(buffer.base);
+//      qDebug()<<decodeHivePacket(buffer.base, &senderAddr[0]);
+//    }
+//  else
+//    {
+//      fprintf(stderr, "UDP Read error %s\n", uv_err_name(nread));
+//      //      uv_close((uv_handle_t*) handle, NULL);
+//    }
+
+//  free(buf->base);
+//  return;
+//}
+
+//void
+//UvServer::udpWriteCb(uv_udp_send_t *handle, int status)
+//{
+//  if(status)
+//    {
+//      fprintf(stderr, "Send error %s\n", uv_strerror(status));
+//      return;
+//    }
+//  free(handle); //<<not sure
+//  Log::net(Log::Normal, "UvServer::udpWriten()", "UDP message successfully sent!");
+//}
+
+//void
+//UvServer::allocBuffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
+//{
+//  buf->base = (char*) malloc(suggested_size);
+//  buf->len = suggested_size;
+//}
+
+//void
+//UvServer::freeWriteReq(uv_write_t *handle)
+//{
+//  write_req_t *wr = (write_req_t*) handle;
+//  free(wr->buf.base);
+//  free(wr);
+//}
+
+//int
+//UvServer::getSocketDescriptor(uv_handle_t *handle)
+//{
+//  int fd;
+//#ifdef Q_OS_WIN
+//  uv_fileno(handle, (uv_os_fd_t*)(&fd));
+//#else
+//  uv_fileno(handle, &fd);
+//#endif
+//  return fd;
+//}
 
 
 
@@ -350,79 +350,79 @@ UvServer::getSocketDescriptor(uv_handle_t *handle)
 ///  - GitHub Source: ultrasilicon/uv_tcp_echo.c
 ///  - URL: https://gist.github.com/ultrasilicon/f070f7acc4fe0b036d37f9383a484f5c
 
-void UvServer::on_new_connection(uv_stream_t *server, int status)
-{
-  if (status < 0) {
-      fprintf(stderr, "New connection error %s\n", uv_strerror(status));
-      // error!
-      return;
-  }
+//void UvServer::on_new_connection(uv_stream_t *server, int status)
+//{
+//  if (status < 0) {
+//      fprintf(stderr, "New connection error %s\n", uv_strerror(status));
+//      // error!
+//      return;
+//  }
 
-  uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
-  uv_tcp_init(loop, client);
-  if (uv_accept(server, (uv_stream_t*) client) == 0) {
-      uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
-  }
-  else {
-      uv_close((uv_handle_t*) client, NULL);
-    }
-}
-
-void UvServer::alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
-{
-  buf->base = (char*) malloc(suggested_size);
-  buf->len = suggested_size;
-}
-
-void UvServer::echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
-{
-  if (nread > 0) {
-      write_req_t *req = (write_req_t*) malloc(sizeof(write_req_t));
-      req->buf = uv_buf_init(buf->base, nread);
-      uv_write((uv_write_t*) req, client, &req->buf, 1, echo_write);
-      return;
-  }
-  if (nread < 0) {
-      if (nread != UV_EOF)
-          fprintf(stderr, "Read error %s\n", uv_err_name(nread));
-      uv_close((uv_handle_t*) client, NULL);
-  }
-
-  free(buf->base);
-}
-
-void UvServer::echo_write(uv_write_t *req, int status)
-{
-  if (status) {
-      fprintf(stderr, "Write error %s\n", uv_strerror(status));
-  }
-  free_write_req(req);
-}
-
-void UvServer::free_write_req(uv_write_t *req)
-{
-  write_req_t *wr = (write_req_t*) req;
-  free(wr->buf.base);
-  free(wr);
-}
-
-void UvServer::timer_cb(uv_timer_t *handle)
-{
-  qDebug()<<"1s";
-//  if(stop)
-//    {
-//      quit();
+//  uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
+//  uv_tcp_init(loop, client);
+//  if (uv_accept(server, (uv_stream_t*) client) == 0) {
+//      uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+//  }
+//  else {
+//      uv_close((uv_handle_t*) client, NULL);
 //    }
-}
+//}
 
-void UvServer::timer_cb2(uv_timer_t *handle)
-{
-  qDebug()<<"0.5s---";
-//  if(stop)
-//    {
-//      quit();
-//    }
-}
+//void UvServer::alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
+//{
+//  buf->base = (char*) malloc(suggested_size);
+//  buf->len = suggested_size;
+//}
+
+//void UvServer::echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
+//{
+//  if (nread > 0) {
+//      write_req_t *req = (write_req_t*) malloc(sizeof(write_req_t));
+//      req->buf = uv_buf_init(buf->base, nread);
+//      uv_write((uv_write_t*) req, client, &req->buf, 1, echo_write);
+//      return;
+//  }
+//  if (nread < 0) {
+//      if (nread != UV_EOF)
+//          fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+//      uv_close((uv_handle_t*) client, NULL);
+//  }
+
+//  free(buf->base);
+//}
+
+//void UvServer::echo_write(uv_write_t *req, int status)
+//{
+//  if (status) {
+//      fprintf(stderr, "Write error %s\n", uv_strerror(status));
+//  }
+//  free_write_req(req);
+//}
+
+//void UvServer::free_write_req(uv_write_t *req)
+//{
+//  write_req_t *wr = (write_req_t*) req;
+//  free(wr->buf.base);
+//  free(wr);
+//}
+
+//void UvServer::timer_cb(uv_timer_t *handle)
+//{
+//  qDebug()<<"1s";
+////  if(stop)
+////    {
+////      quit();
+////    }
+//}
+
+//void UvServer::timer_cb2(uv_timer_t *handle)
+//{
+//  qDebug()<<"0.5s---";
+////  if(stop)
+////    {
+////      quit();
+////    }
+//}
 
 /// END: 9 Dec. 2017 Test
 
