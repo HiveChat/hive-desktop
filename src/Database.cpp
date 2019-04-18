4f7f9888fb341128a189790da8b79df2d4e72493
ultrasilicon <zhenghanecho@gmail.com>
2019-04-17 22:54:23 -0400

    Building database

--- src/Database.cpp
+++ src/Database.cpp
@@ -1,17 +1,29 @@
 #include "Database.h"
 
 #include <QJsonObject>
-
+#include <QJsonArray>
+#include <QStringList>
 
 using namespace DB;
+
+Database::Database()
+{
+}
 
 Database::Database(const std::string &path)
   : path_(path)
 {
 }
 
+Database::~Database()
+{
+
+}
+
 int Database::open()
 {
+  if(path_ == "")
+    return -1;
   if(opened_)
     return -1;
   file_.setPath(path_);
@@ -34,17 +46,81 @@ int Database::close()
   return file_.close(Parsley::Sync);
 }
 
+
+
+
 Map::Map()
+  : Database()
+{
+}
+
+Map::Map(const std::string& path)
+  : Database(path)
+{
+  open();
+  load();
+}
+
+Map::~Map()
 {
 
 }
+
+void Map::load()
+{
+  QJsonObject obj = json_.object();
+  for(auto key : obj.keys())
+    map_.insert(key, obj[key]);
+}
+
+
+
 
 Array::Array()
+  : Database()
+{
+}
+
+Array::Array(const std::string& path)
+  : Database(path)
+{
+  open();
+  load();
+}
+
+Array::~Array()
 {
 
 }
 
+void Array::load()
+{
+  QJsonArray array = json_.array();
+  for(auto val : array)
+    vector_.push_back(val);
+}
+
+
+
+
 ShardedArray::ShardedArray()
+  : Database()
+{
+  open();
+}
+
+ShardedArray::ShardedArray(const std::string& path)
+  : Database(path)
+{
+
+}
+
+ShardedArray::~ShardedArray()
+{
+
+}
+
+void ShardedArray::load()
 {
 
 }
--- src/Database.h
+++ src/Database.h
@@ -4,8 +4,10 @@
 #include <libparsley/file.h>
 
 #include <string>
+#include <vector>
 
 #include <QJsonDocument>
+#include <QHash>
 
 
 namespace DB {
@@ -17,35 +19,62 @@ class ShardedArray;
 class Database
 {
 public:
+  Database();
   Database(const std::string& path);
+  virtual ~Database();
   int open();
   int close();
 
 protected:
+  virtual void load() = 0;
+
+  bool opened_ = false;
   std::string path_;
   Parsley::File file_;
   QJsonDocument json_;
-  bool opened_ = false;
 };
 
 class Map
+    : public Database
 {
 public:
   Map();
+  Map(const std::string& path);
+  virtual ~Map();
 
+protected:
+  virtual void load();
 
+private:
+  QHash<QString, QJsonValue> map_;
 };
 
 class Array
+    : public Database
 {
 public:
   Array();
+  Array(const std::string& path);
+  virtual ~Array();
+
+protected:
+  virtual void load();
+
+private:
+  std::vector<QJsonValue> vector_;
 };
 
 class ShardedArray
+    :public Database
 {
 public:
   ShardedArray();
+  ShardedArray(const std::string& path);
+  virtual ~ShardedArray();
+
+protected:
+  virtual void load();
+
 };
 
 }
